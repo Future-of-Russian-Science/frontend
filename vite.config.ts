ddcb181c62d11573a67c1d2c5cc54c03aeebbf36
Sergey Dyakov <s.dyakov9@gmail.com>
2023-10-01 03:19:05 +0300

    docker fiks

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  preview: {
+    host: true, // Here
+  },
   server: {
     watch: {
       usePolling: true,
